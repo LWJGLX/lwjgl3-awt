4992b3907080394a67c99e9ce61047e26f54959c
httpdigest <kburjack@googlemail.com>
2016-04-29 20:00:03 +0200

    Fix formatting

--- test/org/lwjgl/opengl/awt/AWTTest.java
+++ test/org/lwjgl/opengl/awt/AWTTest.java
@@ -9,37 +9,37 @@ import static org.lwjgl.opengl.GL11.*;
 import javax.swing.JFrame;
 
 public class AWTTest {
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("AWT test");
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setLayout(new BorderLayout());
-		frame.setPreferredSize(new Dimension(600, 600));
-		GLData data = new GLData();
-		data.samples = 4;
-		data.swapInterval = 0;
-		frame.add(new AWTGLCanvas(data) {
-			public void initGL() {
-			    createCapabilities();
-			    glClearColor(0.3f, 0.4f, 0.5f, 1);
-			}
-			public void paintGL() {
-			    int w = getWidth();
-			    int h = getHeight();
-			    float aspect = (float)w/h;
-			    glClear(GL_COLOR_BUFFER_BIT);
-			    glViewport(0, 0, w, h);
-			    glBegin(GL_QUADS);
-			    glColor3f(0.4f, 0.6f, 0.8f);
-			    glVertex2f(-0.75f / aspect, 0.0f);
-			    glVertex2f(0, -0.75f);
-			    glVertex2f(+0.75f / aspect, 0);
-			    glVertex2f(0, +0.75f);
-			    glEnd();
-			    swapBuffers();
-			    this.repaint();
-			}
-		}, BorderLayout.CENTER);
-		frame.pack();
-		frame.setVisible(true);
-	}
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("AWT test");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.setPreferredSize(new Dimension(600, 600));
+        GLData data = new GLData();
+        data.samples = 4;
+        data.swapInterval = 0;
+        frame.add(new AWTGLCanvas(data) {
+            public void initGL() {
+                createCapabilities();
+                glClearColor(0.3f, 0.4f, 0.5f, 1);
+            }
+            public void paintGL() {
+                int w = getWidth();
+                int h = getHeight();
+                float aspect = (float) w / h;
+                glClear(GL_COLOR_BUFFER_BIT);
+                glViewport(0, 0, w, h);
+                glBegin(GL_QUADS);
+                glColor3f(0.4f, 0.6f, 0.8f);
+                glVertex2f(-0.75f / aspect, 0.0f);
+                glVertex2f(0, -0.75f);
+                glVertex2f(+0.75f / aspect, 0);
+                glVertex2f(0, +0.75f);
+                glEnd();
+                swapBuffers();
+                this.repaint();
+            }
+        }, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
--- test/org/lwjgl/opengl/awt/Core32Test.java
+++ test/org/lwjgl/opengl/awt/Core32Test.java
@@ -12,34 +12,35 @@ import static org.lwjgl.opengl.GL30.*;
 import javax.swing.JFrame;
 
 public class Core32Test {
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("AWT test");
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setLayout(new BorderLayout());
-		frame.setPreferredSize(new Dimension(600, 600));
-		GLData data = new GLData();
-		data.samples = 4;
-		data.swapInterval = 0;
-		data.majorVersion = 3;
-		data.minorVersion = 2;
-		data.profile = GLData.Profile.CORE;
-		frame.add(new AWTGLCanvas(data) {
-		    int aspectUniform;
-			public void initGL() {
-			    createCapabilities();
-			    glClearColor(0.3f, 0.4f, 0.5f, 1);
-			    glBindVertexArray(glGenVertexArrays());
-			    int vbo = glGenBuffers();
-			    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-			    glBufferData(GL_ARRAY_BUFFER, new float[] {-0.5f, 0, 0, -0.5f, 0.5f, 0, 0.5f, 0, 0, 0.5f,-0.5f, 0}, GL_STATIC_DRAW);
-			    glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0L);
-			    glEnableVertexAttribArray(0);
-			    int vs = glCreateShader(GL_VERTEX_SHADER);
-			    glShaderSource(vs, "#version 150 core\nuniform float aspect;in vec2 vertex;void main(void){gl_Position=vec4(vertex/vec2(aspect, 1.0), 0.0, 1.0);}");
-			    glCompileShader(vs);
-			    if (glGetShaderi(vs, GL_COMPILE_STATUS) == 0)
-			        throw new AssertionError("Could not compile vertex shader: " + glGetShaderInfoLog(vs));
-			    int fs = glCreateShader(GL_FRAGMENT_SHADER);
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("AWT test");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.setPreferredSize(new Dimension(600, 600));
+        GLData data = new GLData();
+        data.samples = 4;
+        data.swapInterval = 0;
+        data.majorVersion = 3;
+        data.minorVersion = 2;
+        data.profile = GLData.Profile.CORE;
+        frame.add(new AWTGLCanvas(data) {
+            int aspectUniform;
+            public void initGL() {
+                createCapabilities();
+                glClearColor(0.3f, 0.4f, 0.5f, 1);
+                glBindVertexArray(glGenVertexArrays());
+                int vbo = glGenBuffers();
+                glBindBuffer(GL_ARRAY_BUFFER, vbo);
+                glBufferData(GL_ARRAY_BUFFER, new float[] { -0.5f, 0, 0, -0.5f, 0.5f, 0, 0.5f, 0, 0, 0.5f, -0.5f, 0 }, GL_STATIC_DRAW);
+                glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0L);
+                glEnableVertexAttribArray(0);
+                int vs = glCreateShader(GL_VERTEX_SHADER);
+                glShaderSource(vs,
+                        "#version 150 core\nuniform float aspect;in vec2 vertex;void main(void){gl_Position=vec4(vertex/vec2(aspect, 1.0), 0.0, 1.0);}");
+                glCompileShader(vs);
+                if (glGetShaderi(vs, GL_COMPILE_STATUS) == 0)
+                    throw new AssertionError("Could not compile vertex shader: " + glGetShaderInfoLog(vs));
+                int fs = glCreateShader(GL_FRAGMENT_SHADER);
                 glShaderSource(fs, "#version 150 core\nout vec4 color;void main(void){color=vec4(0.4, 0.6, 0.8, 1.0);}");
                 glCompileShader(fs);
                 if (glGetShaderi(fs, GL_COMPILE_STATUS) == 0)
@@ -52,20 +53,20 @@ public class Core32Test {
                     throw new AssertionError("Could not link program: " + glGetProgramInfoLog(prog));
                 glUseProgram(prog);
                 aspectUniform = glGetUniformLocation(prog, "aspect");
-			}
-			public void paintGL() {
-			    int w = getWidth();
-			    int h = getHeight();
-			    float aspect = (float)w/h;
-			    glClear(GL_COLOR_BUFFER_BIT);
-			    glViewport(0, 0, w, h);
-			    glDrawArrays(GL_TRIANGLES, 0, 6);
-			    glUniform1f(aspectUniform, aspect);
-			    swapBuffers();
-			    this.repaint();
-			}
-		}, BorderLayout.CENTER);
-		frame.pack();
-		frame.setVisible(true);
-	}
+            }
+            public void paintGL() {
+                int w = getWidth();
+                int h = getHeight();
+                float aspect = (float) w / h;
+                glClear(GL_COLOR_BUFFER_BIT);
+                glViewport(0, 0, w, h);
+                glUniform1f(aspectUniform, aspect);
+                glDrawArrays(GL_TRIANGLES, 0, 6);
+                swapBuffers();
+                this.repaint();
+            }
+        }, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
