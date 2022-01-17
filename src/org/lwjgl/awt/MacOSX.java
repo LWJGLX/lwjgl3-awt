b92e55cbad5ea80187a0983f06b8dcbdbe49cdb2
SWinxy <swinxythecat@gmail.com>
2022-01-16 21:05:11 -0500

    Delete mac tests I've given up on

--- src/org/lwjgl/awt/MacOSX.java
+++ src/org/lwjgl/awt/MacOSX.java
@@ -1,16 +1,7 @@
 package org.lwjgl.awt;
 
 import org.lwjgl.system.JNI;
-import org.lwjgl.system.MemoryStack;
-import org.lwjgl.system.SharedLibrary;
-import org.lwjgl.system.jawt.JAWTDrawingSurfaceInfo;
-import org.lwjgl.system.jawt.JAWTRectangle;
-import org.lwjgl.system.macosx.MacOSXLibrary;
 import org.lwjgl.system.macosx.ObjCRuntime;
-
-import javax.swing.*;
-import java.awt.*;
-import java.nio.DoubleBuffer;
 
 /**
  * Utility class to provide MacOSX-only stuff.
@@ -19,9 +10,11 @@ import java.nio.DoubleBuffer;
  */
 public class MacOSX {
 
-	// ObjCRuntime does not expose objc_msgSend, so we have to get it ourselves
-	// Pointer to a method that sends a message to an instance of a class
-	// Apple spec: macOS 10.0 (OSX 10; 2001) or higher
+	/**
+	 * ObjCRuntime does not expose objc_msgSend, so we have to get it ourselves
+	 * Pointer to a method that sends a message to an instance of a class
+	 * Apple spec: macOS 10.0 (OSX 10; 2001) or higher
+	 */
 	private static final long objc_msgSend = ObjCRuntime.getLibrary().getFunctionAddress("objc_msgSend");
 
 	// No.
@@ -57,83 +50,5 @@ public class MacOSX {
 		long flush = ObjCRuntime.sel_getUid("flush");
 
 		JNI.invokePPP(CATransaction, flush, objc_msgSend);
-	}
-
-	/**
-	 * Creates a native MacOSX view. TODO
-	 * @param canvas component to get the view from
-	 * @param drawingSurfaceInfo the drawing surface info struct
-	 * @return a pointer to a native window handle
-	 */
-	private static long createMTKView(Canvas canvas, JAWTDrawingSurfaceInfo drawingSurfaceInfo) {
-
-		// if the canvas is inside e.g. a JSplitPane, the dsi coordinates are wrong and need to be corrected
-		JAWTRectangle bounds = drawingSurfaceInfo.bounds();
-		int x = bounds.x();
-		int y = bounds.y();
-
-		JRootPane rootPane = SwingUtilities.getRootPane(canvas);
-		if (rootPane != null) {
-			Point point = SwingUtilities.convertPoint(canvas, new Point(), rootPane);
-			x = point.x;
-			y = point.y;
-		}
-
-		// Get the preferred default Metal device object
-		// Apple spec: 10.11 (OSX El Capitan; 2018) or higher
-		SharedLibrary metal = MacOSXLibrary.getWithIdentifier("com.apple.Metal");
-		long pMTLCreateSystemDefaultDevice =  metal.getFunctionAddress("MTLCreateSystemDefaultDevice");
-		long pDevice = JNI.invokeP(pMTLCreateSystemDefaultDevice);
-
-		// surfaceLayers.windowLayer.frame.size.height
-		// TODO: get height of the internal window frame via Obj-C bindings
-		y = -bounds.height() - y;
-
-		try (MemoryStack stack = MemoryStack.stackPush()) {
-			DoubleBuffer frame = stack.doubles(x, y, bounds.width(), bounds.height());
-
-			// MTKView *view = [[MTKView alloc] initWithFrame:frame device:device];
-
-			// surfaceLayers.layer = view.layer;
-			// return view.layer
-			return 0;
-		}
-	}
-
-	/**
-	 * Creates a native NSOpenGLView. TODO
-	 * @param canvas component to get the view from
-	 * @param drawingSurfaceInfo the drawing surface info struct
-	 * @param pixelFormat
-	 * @return a pointer to a native window handle
-	 */
-	private static long createOpenGLView(Canvas canvas, JAWTDrawingSurfaceInfo drawingSurfaceInfo, long pixelFormat) {
-
-		// if the canvas is inside e.g. a JSplitPane, the dsi coordinates are wrong and need to be corrected
-		JAWTRectangle bounds = drawingSurfaceInfo.bounds();
-		int x = bounds.x();
-		int y = bounds.y();
-
-		JRootPane rootPane = SwingUtilities.getRootPane(canvas);
-		if (rootPane != null) {
-			Point point = SwingUtilities.convertPoint(canvas, new Point(), rootPane);
-			x = point.x;
-			y = point.y;
-		}
-
-		// surfaceLayers.windowLayer.frame.size.height
-		// TODO: get height of the internal window frame via Obj-C bindings
-		y = - bounds.height() - y;
-
-		try (MemoryStack stack = MemoryStack.stackPush()) {
-			DoubleBuffer frame = stack.doubles(x, y, bounds.width(), bounds.height());
-
-			// NSOpenGLView *view = [[NSOpenGLView alloc] initWithFrame:frame pixelFormat:(id)pixelFormat];
-			// [view setWantsLayer:YES];
-
-			// surfaceLayers.layer = view.layer;
-			// return view.layer
-			return 0;
-		}
 	}
 }
